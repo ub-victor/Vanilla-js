652a099c91541ec70b4d32ea60f1006100248904
ub-victor <victoireushindi371@gmail.com>
2025-05-28 01:14:15 +0200

    Initial commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function smoothScroll(target,duration){ //
     var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect().top;
     console.log(targetPosition);
+    console.log(target);
 }
 
 smoothScroll(".section1", 1000);
